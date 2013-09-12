72b15ced041bc1ab77b4a074c4618524cbd9ed1c
Mitchell Kutchuk <mitch528@gmail.com>
2013-09-11 20:21:51 -0700

    Added some classes from old branch

---
+++ TrinityCore-Manager/Clients/LocalClient.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrinityCore_Manager.Misc;
+using TrinityCore_Manager.TCM;
+
+namespace TrinityCore_Manager.Clients
+{
+    public class LocalClient : TCMClient
+    {
+
+        public event EventHandler ClientExited;
+        public event EventHandler<ClientReceivedDataEventArgs> DataReceived;
+
+        private Process _proc;
+
+        public Process UnderlyingProcess
+        {
+            get
+            {
+                return _proc;
+            }
+        }
+
+        public int UnderlyingProcessId
+        {
+            get
+            {
+                return _procId;
+            }
+        }
+
+        private int _procId = -1;
+
+        private readonly string _exeFile;
+        private readonly string _exeArgs;
+
+        public LocalClient(string exeFile, string args = "")
+        {
+            _exeFile = exeFile;
+            _exeArgs = args;
+        }
+
+        public override bool IsOnline
+        {
+            get { return _proc != null && !_proc.HasExited && ProcessHelper.ProcessExists(_procId); }
+        }
+
+        public override void Start()
+        {
+
+            _proc = ProcessHelper.StartProcess(_exeFile, Path.GetDirectoryName(_exeFile), _exeArgs);
+            _procId = _proc.Id;
+
+            _proc.Exited += (sender, e) =>
+            {
+                if (ClientExited != null)
+                    ClientExited(this, EventArgs.Empty);
+            };
+
+            _proc.BeginOutputReadLine();
+            _proc.BeginErrorReadLine();
+
+            _proc.OutputDataReceived += ProcessStandard;
+            _proc.ErrorDataReceived += ProcessError;
+
+        }
+
+        private void ProcessStandard(object sender, DataReceivedEventArgs e)
+        {
+            if (DataReceived != null)
+                DataReceived(this, new ClientReceivedDataEventArgs(e.Data));
+        }
+
+        private void ProcessError(object sender, DataReceivedEventArgs e)
+        {
+            if (DataReceived != null)
+                DataReceived(this, new ClientReceivedDataEventArgs(e.Data));
+        }
+
+        public override void Stop()
+        {
+            ProcessHelper.KillProcess(_procId);
+        }
+
+        public override async Task SendMessage(string message)
+        {
+
+            StreamWriter writer = _proc.StandardInput;
+
+            await writer.WriteLineAsync(message);
+
+        }
+
+    }
+
+    public class ClientReceivedDataEventArgs : EventArgs
+    {
+
+        public string Data { get; set; }
+
+        public ClientReceivedDataEventArgs(string data)
+        {
+            Data = data;
+        }
+
+    }
+
+}
---
+++ TrinityCore-Manager/Clients/TCPClient.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TrinityCore_Manager.TCM;
+
+namespace TrinityCore_Manager.Clients
+{
+    class TCPClient : TCMClient, IDisposable
+    {
+
+        public event EventHandler TCConnected;
+        public event EventHandler TCDisconnected;
+
+        public event MessageReceivedEventHandler TCMessageReceived;
+
+        private TcpClient _client;
+
+        private byte[] _buffer;
+
+        private string _host;
+        private int _port;
+
+        public bool IsConnected { get; private set; }
+
+        public TCPClient(string host, int port)
+        {
+
+            _host = host;
+            _port = port;
+
+            _buffer = new byte[1024];
+
+        }
+
+        /// <summary>
+        /// Connect To Server
+        /// </summary>
+        public void Connect()
+        {
+
+            if (_client != null)
+            {
+                throw new Exception("_client NOT null");
+            }
+
+            _client = new TcpClient();
+
+            _client.BeginConnect(_host, _port, Connected, _client);
+
+        }
+
+        /// <summary>
+        /// Disconnect from server
+        /// </summary>
+        public void Disconnect()
+        {
+
+            if (_client == null)
+                return;
+
+            IsConnected = false;
+
+            if (TCDisconnected != null)
+                TCDisconnected(this, EventArgs.Empty);
+
+            _client.Close();
+
+            _client = null;
+            _buffer = null;
+
+
+        }
+
+        private void Connected(IAsyncResult iar)
+        {
+
+            try
+            {
+
+                if (_client == null)
+                    return;
+
+                _client.EndConnect(iar);
+
+                if (_client.Connected)
+                {
+
+                    var stream = _client.GetStream();
+
+                    stream.BeginRead(_buffer, 0, _buffer.Length, Receive, stream);
+
+                }
+
+            }
+            catch (Exception)
+            {
+                Disconnect();
+            }
+
+        }
+
+        private void Receive(IAsyncResult iar)
+        {
+
+            try
+            {
+
+                var stream = iar.AsyncState as NetworkStream;
+
+                if (stream == null)
+                    return;
+
+                int read = stream.EndRead(iar);
+
+                ProcessReceive(read);
+
+            }
+            catch (Exception)
+            {
+                Disconnect();
+            }
+
+        }
+
+        private void ProcessReceive(int read)
+        {
+
+            try
+            {
+
+                if (!IsConnected)
+                {
+
+                    IsConnected = true;
+
+                    if (TCConnected != null)
+                        TCConnected(this, EventArgs.Empty);
+
+                }
+
+                string str = Encoding.UTF8.GetString(_buffer, 0, read);
+
+                string[] ex = Regex.Split(str, "\r\n");
+
+                var stream = _client.GetStream();
+
+
+                if (String.IsNullOrEmpty(str) || read == 0)
+                {
+                    stream.BeginRead(_buffer, 0, _buffer.Length, Receive, stream);
+                }
+                else if (ex.Length > 0)
+                {
+
+                    for (int i = 0; i < ex.Length; i++)
+                    {
+
+                        if (String.IsNullOrEmpty(ex[i]) || ex[i] == "\n")
+                            continue;
+
+                        if (TCMessageReceived != null)
+                            TCMessageReceived(this, new MessageReceivedEventArgs(ex[i]));
+
+                    }
+
+                    _buffer = new byte[1024];
+
+                    stream.BeginRead(_buffer, 0, _buffer.Length, Receive, stream);
+
+                }
+                else //need more data
+                {
+
+                    byte[] tmp = Encoding.UTF8.GetBytes(str);
+
+                    _buffer = new byte[read + 1024];
+                    Array.Copy(tmp, 0, _buffer, 0, tmp.Length);
+
+                    stream.BeginRead(_buffer, read, _buffer.Length - read, Receive, stream);
+
+                }
+
+            }
+            catch (Exception)
+            {
+                Disconnect();
+            }
+
+        }
+
+        /// <summary>
+        /// Send a message to the server
+        /// </summary>
+        /// <param name="message">The message to send</param>
+        public async Task Send(string message)
+        {
+
+            try
+            {
+
+                if (_client == null)
+                    return;
+
+                if (!_client.Connected || !IsConnected)
+                    return;
+
+                byte[] msg = Encoding.UTF8.GetBytes(message + "\r\n");
+
+                var stream = _client.GetStream();
+
+                await Task.FromResult(stream.BeginWrite(msg, 0, msg.Length, Send, stream));
+
+            }
+            catch (Exception)
+            {
+                Disconnect();
+            }
+
+        }
+
+        private void Send(IAsyncResult iar)
+        {
+
+            try
+            {
+
+                var stream = iar.AsyncState as NetworkStream;
+
+                if (stream == null)
+                    return;
+
+                stream.EndWrite(iar);
+
+            }
+            catch (Exception)
+            {
+                Disconnect();
+            }
+
+        }
+
+        public override bool IsOnline
+        {
+            get { return IsConnected; }
+        }
+
+        public override void Start()
+        {
+            Connect();
+        }
+
+        public override void Stop()
+        {
+            Disconnect();
+        }
+
+        public override async Task SendMessage(string message)
+        {
+            await Send(message);
+        }
+
+        public void Dispose()
+        {
+
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+
+            if (disposing)
+            {
+                if (_client != null)
+                    _client.Close();
+            }
+
+            _client = null;
+            _buffer = null;
+
+        }
+
+        ~TCPClient()
+        {
+            Dispose(false);
+        }
+
+    }
+
+    public delegate void MessageReceivedEventHandler(object sender, MessageReceivedEventArgs e);
+
+    public class MessageReceivedEventArgs : EventArgs
+    {
+
+        public string Message { get; set; }
+
+        public MessageReceivedEventArgs(string message)
+        {
+            Message = message;
+        }
+
+    }
+
+}
