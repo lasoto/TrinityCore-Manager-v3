791c9f2edff26c0602490a54279f4ebca0270619
Mitchell Kutchuk <mitch528@gmail.com>
2013-09-13 09:52:02 -0700

    Switching to Fluent ribbon library

--- TrinityCore-Manager/MainWindow.xaml.cs
+++ TrinityCore-Manager/MainWindow.xaml.cs
@@ -4,39 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Automation.Peers;
-using System.Windows.Automation.Provider;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Fluent;
 
 namespace TrinityCore_Manager
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow2.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow2 : RibbonWindow
     {
-        public MainWindow()
+        public MainWindow2()
         {
             InitializeComponent();
-        }
-
-        private void CommandTextBox_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                ButtonAutomationPeer peer = new ButtonAutomationPeer(ExecuteCmdButton);
-                IInvokeProvider invokeProv = peer.GetPattern(PatternInterface.Invoke) as IInvokeProvider;
-                invokeProv.Invoke();
-                FocusManager.SetFocusedElement(this, CommandTextBox);
-                Keyboard.Focus(CommandTextBox);
-            }
         }
     }
 }
---
+++ TrinityCore-Manager/MainWindowOld.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrinityCore_Manager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                ButtonAutomationPeer peer = new ButtonAutomationPeer(ExecuteCmdButton);
+                IInvokeProvider invokeProv = peer.GetPattern(PatternInterface.Invoke) as IInvokeProvider;
+                invokeProv.Invoke();
+                FocusManager.SetFocusedElement(this, CommandTextBox);
+                Keyboard.Focus(CommandTextBox);
+            }
+        }
+    }
+}
