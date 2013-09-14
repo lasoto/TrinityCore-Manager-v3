17634b1dc4008018190f5332e563cf1d052fabb4
Mitchell Kutchuk <mitch528@gmail.com>
2013-09-13 19:15:48 -0700

    Working on Setup Wizard

---
+++ TrinityCore-Manager/ViewModels/SetupWizardViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catel.Data;
+using Catel.MVVM;
+using Catel.MVVM.Services;
+using TrinityCore_Manager.ViewModels.Wizard;
+using Xceed.Wpf.Toolkit;
+using Xceed.Wpf.Toolkit.Core;
+
+namespace TrinityCore_Manager.ViewModels
+{
+    [InterestedIn(typeof(WizardConnectOptionViewModel))]
+    public class SetupWizardViewModel : ViewModelBase
+    {
+
+        private readonly IUIVisualizerService _uiVisualizerService;
+        private readonly IPleaseWaitService _pleaseWaitService;
+
+        public Command<CancelRoutedEventArgs> Next { get; private set; }
+
+        public SetupWizardViewModel(IUIVisualizerService uiVisualizerService, IPleaseWaitService pleaseWaitService)
+        {
+
+            _uiVisualizerService = uiVisualizerService;
+            _pleaseWaitService = pleaseWaitService;
+
+            Next = new Command<CancelRoutedEventArgs>(NextButtonPressed);
+
+        }
+
+        private void NextButtonPressed(CancelRoutedEventArgs e)
+        {
+
+            
+
+        }
+
+        protected override void OnViewModelPropertyChanged(IViewModel viewModel, string propertyName)
+        {
+
+            if (viewModel is WizardConnectOptionViewModel)
+            {
+
+                var model = (WizardConnectOptionViewModel)viewModel;
+
+                Console.WriteLine(Page.Name);
+
+            }
+
+
+        }
+
+        public WizardPage Page
+        {
+            get
+            {
+                return GetValue<WizardPage>(PageProperty);
+            }
+            set
+            {
+                SetValue(PageProperty, value);
+            }
+        }
+
+        public static readonly PropertyData PageProperty = RegisterProperty("Page", typeof(WizardPage));
+
+    }
+}
---
+++ TrinityCore-Manager/Views/Wizard/WizardConnectOptionView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using TrinityCore_Manager.ViewModels.Wizard;
+
+namespace TrinityCore_Manager.Views.Wizard
+{
+    using Catel.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for WizardConnectOptionPage.xaml.
+    /// </summary>
+    public partial class WizardConnectOptionView : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WizardConnectOptionView"/> class.
+        /// </summary>
+        public WizardConnectOptionView()
+        {
+            InitializeComponent();
+        }
+
+        protected override System.Type GetViewModelType()
+        {
+            return typeof(WizardConnectOptionViewModel);
+        }
+
+    }
+}
